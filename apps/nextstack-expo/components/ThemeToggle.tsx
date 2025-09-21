7da1039f47b2661fe5ab8c77f23caf61ec184393
feidaZhang <feida.zhang@storehub.com>
2025-09-21 23:24:35 +0800

    fix: update

--- apps/nextstack-expo/components/ThemeToggle.tsx
+++ apps/nextstack-expo/components/ThemeToggle.tsx
@@ -1,27 +1,28 @@
-import { Pressable, View } from 'react-native'
-import { Moon, Sun } from 'lucide-react-native'
-import { useColorScheme } from '@/lib/useColorScheme'
+import { Moon, Sun } from 'lucide-react-native';
+import { Pressable, View } from 'react-native';
+
+import { useColorScheme } from '@/lib/useColorScheme';
 
 export function ThemeToggle() {
-  const { isDarkColorScheme, setColorScheme } = useColorScheme()
+  const { isDarkColorScheme, setColorScheme } = useColorScheme();
 
   const toggleColorScheme = () => {
-    const newTheme = isDarkColorScheme ? 'light' : 'dark'
-    setColorScheme(newTheme)
-  }
+    const newTheme = isDarkColorScheme ? 'light' : 'dark';
+    setColorScheme(newTheme);
+  };
 
   return (
     <Pressable
       onPress={toggleColorScheme}
-      className="web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2 active:opacity-70"
+      className='web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2 active:opacity-70'
     >
-      <View className="flex-1 aspect-square pt-0.5 justify-center items-center web:px-5">
+      <View className='flex-1 aspect-square pt-0.5 justify-center items-center web:px-5'>
         {isDarkColorScheme ? (
-          <Moon className="text-foreground" size={23} strokeWidth={1.25} />
+          <Moon className='text-foreground' size={23} strokeWidth={1.25} />
         ) : (
-          <Sun className="text-foreground" size={24} strokeWidth={1.25} />
+          <Sun className='text-foreground' size={24} strokeWidth={1.25} />
         )}
       </View>
     </Pressable>
-  )
+  );
 }
